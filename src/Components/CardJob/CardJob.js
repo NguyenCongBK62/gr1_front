9ce3acd6641f853628941f2b668028788ce7a208
CongNV-Training <nguyen.van.cong@sun-asterisk.com>
2021-09-12 09:55:26 +0700

    use axios

--- src/Components/CardJob/CardJob.js
+++ src/Components/CardJob/CardJob.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './CardJob.css';
 import {DollarCircleOutlined} from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectJob } from '../../actions/job';
+import { selectJob } from '../../redux/job';
 
 const CardJob = (props) => {
 
